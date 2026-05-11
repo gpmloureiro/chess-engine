4a05d2e23a09fae831f78bb9406fa8811e40771c
Guilherme Loureiro <gpmloureiro@gmail.com>
2026-05-11 14:54:54 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Chess Engine");
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+
+        // Draw here
+
+        window.display();
+    }
+}
